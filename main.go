35ab685496338d5e5d3d1d7680881159ca2f64a3
obskur123 <tomasxd768@outlook.com>
2023-09-08 15:24:06 -0300

    agregue la libreria de colores porque me parecio graciosa

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"github.com/fatih/color"
 	"github.com/gin-gonic/gin"
 	"github.com/obskur123/crent/src/handlers"
 )
 
 func main() {
+	color.Green("ᕕ( ᐛ )ᕗ gah mf damn dawg! starting the server real quick...")
 	r := gin.Default()
 	r.GET("/ping", handlers.Ping)
 	r.Run()
