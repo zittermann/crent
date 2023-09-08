49232513bf8ab02f3bc8d0846914dff0bcb3fdfc
obskur123 <tomasxd768@outlook.com>
2023-09-08 15:14:33 -0300

    cambie fiber x gin porque german se caga encima

--- main.go
+++ main.go
@@ -1,32 +1,12 @@
 package main
 
 import (
-	"log"
-
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/template/pug/v2"
-	// "net/http" // embedded system
+	"github.com/gin-gonic/gin"
+	"github.com/obskur123/crent/src/handlers"
 )
 
 func main() {
-	// Create a new engine
-	engine := pug.New("./views", ".pug")
-
-	// Or from an embedded system
-	// See github.com/gofiber/embed for examples
-	// engine := pug.NewFileSystem(http.Dir("./views", ".pug"))
-
-	// Pass the engine to the views
-	app := fiber.New(fiber.Config{
-		Views: engine,
-	})
-
-	app.Get("/", func(c *fiber.Ctx) error {
-		// Render index
-		return c.Render("index", fiber.Map{
-			"msg": "Hello, World!",
-		})
-	})
-
-	log.Fatal(app.Listen(":3000"))
+	r := gin.Default()
+	r.GET("/ping", handlers.Ping)
+	r.Run()
 }
