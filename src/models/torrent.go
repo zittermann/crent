af06117831a60cfea7a161c9a38eccd783b33542
Germán Zittermann <germanzittermann@gmail.com>
2023-10-09 11:16:05 -0300

    Annotations added to User and Torrents structs

--- src/models/torrent.go
+++ src/models/torrent.go
@@ -1,11 +1,11 @@
 package models
 
 type Torrent struct {
-	ID uint
-	Title string
-	Likes uint
-	Dislikes uint
-	URI string
-	Image string
-	Category string
+	ID uint `json:"id,omitempty" gorm:"column:id;primarykey"`
+	Title string `json:"title,omitempty" gorm:"not null"`
+	Likes uint `json:"likes,omitempty"`
+	Dislikes uint `json:"dislikes,omitempty"`
+	URI string `json:"uri,omitempty" gorm:"not null"`
+	Image string `json:"img,omitempty"`
+	Category string `json:"category,omitempty"`
 }
