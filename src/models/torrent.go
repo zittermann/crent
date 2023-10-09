e0c10e404231a164a0c951ebeda87b9f69b71fe3
Germán Zittermann <germanzittermann@gmail.com>
2023-10-09 12:43:12 -0300

    ngl i dont remember what i changed

--- src/models/torrent.go
+++ src/models/torrent.go
@@ -4,7 +4,7 @@ import "gopkg.in/guregu/null.v4"
 
 type Torrent struct {
 	ID uint `json:"id,omitempty" gorm:"column:id;primarykey"`
-	Title null.String `json:"title,omitempty" gorm:"not null"`
+	Title string `json:"title,omitempty" gorm:"not null"`
 	Likes uint `json:"likes,omitempty"`
 	Dislikes uint `json:"dislikes,omitempty"`
 	URI string `json:"uri,omitempty" gorm:"not null"`
